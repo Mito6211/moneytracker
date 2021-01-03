88e1e70188f734c5a2b89e4567e79c81edf2e14b
Mito6211 <willmoore674@gmail.com>
2021-01-02 22:04:59 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Arrow from "./Arrow";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="top-money-display">
+                <span className="income stroke-text">$3100</span>
+                <Arrow />
+                <span className="total stroke-text">$123,456,789</span>
+                <Arrow />
+                <span className="expenses stroke-text">$1200</span>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Arrow.js
@@ -0,0 +1,10 @@
+const Arrow = () => (
+    <svg className="arrow" width="34" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M32.884 10.884a1.25 1.25 0 000-1.768l-7.955-7.955A1.25 1.25 0 1023.16 2.93L30.232 10l-7.07 7.071a1.25 1.25 0 101.767 1.768l7.955-7.955zM0 11.25h32v-2.5H0v2.5z"
+            fill="#000"
+        />
+    </svg>
+);
+
+export default Arrow;
